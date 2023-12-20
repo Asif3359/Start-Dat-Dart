f051e1750bc56b4fb7c3f084535aede636106e14
Asif3359 <asifahammed359@gmail.com>
2023-12-20 20:31:06 +0600

    add link in constant variable

--- main.dart
+++ main.dart
@@ -1,6 +1,8 @@
 main() {
+  const link =
+      'https://www.youtube.com/playlist?list=PLlxmoA0rQ-LyHW9voBdNo4gEEIh0SjG-q';
   print("Hello World");
   print("This Is My First Dart Code ");
-  print(
-      "playlist i flow :https://www.youtube.com/playlist?list=PLlxmoA0rQ-LyHW9voBdNo4gEEIh0SjG-q ");
+  print("playlist i flow : ");
+  print(link);
 }
