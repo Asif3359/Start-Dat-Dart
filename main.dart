8ac17e4e9a55a1ee47774b9e740e9e9d2cb158ee
Asif3359 <asifahammed359@gmail.com>
2023-12-23 19:57:53 +0600

    dart normal function and arrow function

--- main.dart
+++ main.dart
@@ -5,4 +5,18 @@ main() {
   print("This Is My First Dart Code ");
   print("playlist i flow : ");
   print(link);
+  print("");
+  int result = sum(2, 4);
+  int multiply = multiplication(2, 3);
+  print("Sum = $result, \nMultiplication = $multiply");
 }
+
+// datr function
+
+// dart nirmal dunction
+int sum(number1, number2) {
+  return number1 + number1;
+}
+
+// arow function in dart
+int multiplication(n1, n2) => n1 + n2;
