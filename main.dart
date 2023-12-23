8971d9bf2d621769b468c9a772a2334cfed6fa15
Asif3359 <asifahammed359@gmail.com>
2023-12-23 20:30:09 +0600

    pass function in function its working

--- main.dart
+++ main.dart
@@ -9,14 +9,14 @@ main() {
   int result = sum(2, 4);
   int multiply = multiplication(2, 3);
   int teoRes = twoResult(sum(2, 5), multiplication(2, 2));
-  print("Sum = $result, \nMultiplication = $multiply, \nTwoMinus = $teoRes,");
+  print("Sum = $result, \nMultiplication = $multiply, \nTwoMinus = $teoRes");
 }
 
 // datr function
 
 // dart nirmal dunction
 int sum(number1, number2) {
-  return number1 + number1;
+  return number1 + number2;
 }
 
 // arow function in dart
